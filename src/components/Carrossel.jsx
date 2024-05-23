acd5b8430f2ae17c7765259da6fbd8b04fbe1eec
vitera <victorg355@outlook.com>
2024-05-23 11:43:27 -0300

    add carrossel

---
+++ src/components/Carrossel.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect, useRef} from 'react';
+import { motion } from 'framer-motion';
+
+
+
+
+ const myImages = [
+    'img/Carrossel/img001.jpg',
+    'img/Carrossel/img002.jpg',
+    'img/Carrossel/img003.jpg',
+    'img/Carrossel/img004.jpg',
+];
+
+function Carrossel() {
+
+  const carrossel = useRef();
+  const [sizeWindow, setSizeWindow] = useState(0);
+
+  useEffect(() => {
+  
+    setSizeWindow(carrossel.current?.scrollWidth - carrossel.current?.offsetWidth);
+  })
+
+  return (
+    <div className="batata">
+      {}
+      <motion.div className='carrosselContainer' ref={carrossel} whileTap={{cursor: 'grabbing'}}>
+        <motion.div 
+          className='carrossel'
+          drag='x'
+          dragConstraints={{right: 0, left: -sizeWindow}}
+          initial={{x: 100}}
+          animate={{x: 0}}
+          transition={{duration: 0.6}}
+        >
+
+          {myImages.map((img, index) => (
+            <motion.div key={index} className='image'>
+              <img src={img} alt='Nome Imagem' />
+            </motion.div>
+          ))}
+
+        </motion.div>
+      </motion.div>
+
+    </div>
+  );
+}
+
+export default Carrossel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import HeroSection from "../components/HeroSection";
 import DiscoveryPlaces from "../components/DiscoveryPlaces";
+import Carrossel from "../components/Carrossel";
+
 
 export default function Home() {
 
     return(
         <div>
+            <Carrossel />
             <HeroSection />
             <DiscoveryPlaces />
         </div>
